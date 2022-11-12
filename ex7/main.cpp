090cff806a1697fac739e20b5c074e936b184f0a
Luan-MB <luanmb20@gmail.com>
2022-11-12 18:37:00 -0300

    terminado

--- ex7/main.cpp
+++ ex7/main.cpp
@@ -1,4 +1,7 @@
+// Luan Machado Bernardt
+
 #include <iostream>
+#include <iomanip>
 #include <list>
 
 #include "Pessoa.hpp"
@@ -9,32 +12,36 @@ int main() {
 
     Curso c1{"BCC", 2011, 4800};
 
-	Pessoa *p0{new Pessoa{"Olavo de Carvalho", 11111111111, 45}};
+	Pessoa *p0{new Pessoa{"Professor", 11111111111, 45}};
 	
 	Disciplina d1{"Orientacao a Objetos", c1, p0};
 
     std::list<Pessoa*> lista;
 
-    Pessoa* p1{new Pessoa{"Michael Jackson", 0, 6}};
-    Pessoa* p2{new Pessoa{"Amy Winehouse", 11111111111, 6}};
-    Pessoa* p3{new Pessoa{"Kurt Cobain", 55555555555, 10}};
-    Pessoa* p4{new Pessoa{"Freddie Mercury", 8286853989, 10}};
+    Pessoa* p1{new Pessoa{"Aluno1", 0, 6}};
+    Pessoa* p2{new Pessoa{"Aluno2", 11111111111, 6}};
+    Pessoa* p3{new Pessoa{"Aluno3", 55555555555, 10}};
+    Pessoa* p4{new Pessoa{"Aluno4", 8286853989, 10}};
     
     d1.adicionarAluno(p1);
     d1.adicionarAluno(p2);
     d1.adicionarAluno(p3);
     d1.adicionarAluno(p4);
 
+    std::cout << "--- Lista apos insercoes ---" << std::endl;
+    
     for (Pessoa* p: d1.getAlunos()) {
-        std::cout << p->getNome() << std::endl;
+        std::cout << p->getNome() << '\t' << std::setfill('0') << std::setw(11) << p->getCpf() << std::endl;
     }
+
 
     d1.removeAluno(p3);
     d1.removeAluno(8286853989);
 
-    std::cout << '\n';
+    std::cout << "--- Lista apos remocoes ---" << std::endl;
+
     for (Pessoa* p: d1.getAlunos()) {
-        std::cout << p->getNome() << std::endl;
+        std::cout << p->getNome() << '\t' << std::setfill('0') << std::setw(11) << p->getCpf() << std::endl;
     }
 
     delete p0;
