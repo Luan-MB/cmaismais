0cf27cc61f60c01d6ca88bd6ab2d3848f9703a7f
Luan-MB <luanbernardt@hotmail.com>
2022-12-13 14:24:01 -0300

    ex12

---
+++ ex12/Fila.hpp
@@ -0,0 +1,49 @@
+#ifndef FILA_HPP
+#define FILA_HPP
+    
+#include <iostream>
+
+template <typename T, std::size_t MAX_SIZE>
+
+class Fila {
+    public:
+        Fila()
+            :inicio{0}, fim{0} {
+        }
+
+        ~Fila() {
+        }
+
+        bool empty() {
+            return(this->inicio == this->fim);
+        }
+
+        bool full() {
+            return(((this->inicio + MAX_SIZE) == this->fim));
+        }
+
+        bool remove(T* const elemento) {
+            if (this->empty())
+                return false;
+            *elemento = this->fila[this->inicio % MAX_SIZE];
+            this->inicio++;
+            return true;
+        }
+
+        bool append(const T elemento) {
+            if (this->full()) {
+                return false;
+            }
+            this->fila[this->fim % MAX_SIZE] = elemento;
+            this->fim++;
+            return true;
+        }
+
+    private:
+        int inicio;
+        int fim;
+        T fila[MAX_SIZE];
+
+};
+
+#endif
---
+++ ex12/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "Fila.hpp"
+
+int main(){
+	int retorno;
+	
+	Fila<int, 10> f;
+
+	f.append(1);
+	f.append(1);
+	f.append(2);
+	f.append(3);
+	f.append(5);
+	f.append(8);
+	f.append(13);
+
+	while (!f.empty()) {
+		f.remove(&retorno);
+		std::cout << retorno << "\n";
+	}
+
+	std::cout << "Fim\n";
+
+	return 0;
+}
