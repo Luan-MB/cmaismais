7662e9e105a19f0dc458110c7cd5993b4c5837b7
Luan-MB <luanbernardt@hotmail.com>
2023-01-24 14:30:56 -0300

    ex16 feito

--- ex16/main.cpp
+++ ex16/main.cpp
@@ -2,33 +2,55 @@
 #include <stdexcept>
 
 #include "CPFInvalidoException.hpp"
+#include "AlunoInexistenteException.hpp"
 #include "Pessoa.hpp"
+#include "Disciplina.hpp"
 
 int main() {
-  Pessoa *p{nullptr};
   std::string nome;
   unsigned long cpf;
   unsigned short int idade;
 
-  std::cout << "Digite o nome: ";
-  std::cin >> nome;
-  std::cout << "Digite o cpf: ";
+  Disciplina d{"C++"};
+
+  for (int i=0; i<3; i++) {
+
+    std::cout << "----------------" << std::endl;
+    std::cout << "Digite o nome: ";
+    std::cin >> nome;
+    std::cout << "Digite o cpf: ";
+    std::cin >> cpf;
+    std::cout << "Digite a idade: ";
+    std::cin >> idade;
+
+    try {
+      Pessoa p{nome, cpf, idade};
+      std::cout << p.getNome() << " " << p.getCpf() << " " << p.getIdade()
+                << std::endl;
+      d.adicionarAluno(&p);
+    } catch (std::invalid_argument &iv) {
+      std::cout << "Argumento invalido: " << iv.what() << std::endl;
+    } catch (CPFInvalidoException &cpfe) {
+      std::cout << "Erro de CPF: " << cpfe.what() << cpfe.cpf << std::endl;
+    } catch (std::exception &ex) {
+      std::cout << "Erro Generico: " << ex.what() << std::endl;
+    }
+  }
+
+  std::cout << "----------------" << std::endl;
+  std::cout << "Digite o cpf do aluno a ser removido: ";
   std::cin >> cpf;
-  std::cout << "Digite a idade: ";
-  std::cin >> idade;
 
   try {
-    p = new Pessoa{nome, cpf, idade};
-    std::cout << p->getNome() << " " << p->getCpf() << " " << p->getIdade()
-              << std::endl;
+    d.removerAluno(cpf);
+    std::cout << "Aluno removido." << cpf << std::endl;
   } catch (std::invalid_argument &iv) {
     std::cout << "Argumento invalido: " << iv.what() << std::endl;
-  } catch (CPFInvalidoException &cpfe) {
-    std::cout << "Erro de CPF: " << cpfe.what() << cpfe.cpf << std::endl;
+  } catch (AlunoInexistenteException &aie) {
+    std::cout << "Erro de CPF: " << aie.what() << aie.cpf << std::endl;
   } catch (std::exception &ex) {
     std::cout << "Erro Generico: " << ex.what() << std::endl;
   }
 
-  delete p;
   return 0;
 }
